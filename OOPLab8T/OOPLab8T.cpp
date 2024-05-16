0d6ab1c33929275768ce3f0d3a9ca23b0a226709
Radyslav-Paskariuk <paskariuk.radyslav@chnu.edu.ua>
2024-05-16 10:59:14 +0300

    second

--- OOPLab8T/OOPLab8T.cpp
+++ OOPLab8T/OOPLab8T.cpp
@@ -14,6 +14,34 @@ const char* maxElement(const char* a, const char* b) {
     return (std::strcmp(a, b) > 0) ? a : b;
 }
 
+// #2
+template<typename T>
+void shellSort(T arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            T temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+template<>
+void shellSort<char*>(char* arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            char* temp = arr[i];
+            int j;
+            for (j = i; j >= gap && std::strcmp(arr[j - gap], temp) > 0; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
 
 int main()
 {
@@ -26,5 +54,24 @@ int main()
 
 
     return 0;
+
+    // #2
+    int intArr[] = { 12, 34, 54, 2, 3 };
+    int intSize = sizeof(intArr) / sizeof(intArr[0]);
+    shellSort(intArr, intSize);
+    std::cout << "Sorted integer array: ";
+    for (int i = 0; i < intSize; ++i) {
+        std::cout << intArr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    const char* charArr[] = { "apple", "banana", "orange", "grape", "pineapple" };
+    int charSize = sizeof(charArr) / sizeof(charArr[0]);
+    shellSort(charArr, charSize);
+    std::cout << "Sorted string array: ";
+    for (int i = 0; i < charSize; ++i) {
+        std::cout << charArr[i] << " ";
+    }
+    std::cout << std::endl;
 
 }
